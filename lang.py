9a1fe125a0f6a47cfc5c0c3db3d7df04503d2753
Yury Petrov <jpsbur@gmail.com>
2016-11-26 19:22:08 +0300

    refactor: lang keys lex-order

--- lang.py
+++ lang.py
@@ -1,32 +1,32 @@
 lang_ru = {
-    'index_title': 'Шарики',
-    'index_no_events': 'Нет соревнований',
-    'index_not_authorised': 'Вы не авторизованы.',
-    'index_log_in': 'Войти',
-    'index_monitor_url': 'URL монитора',
-    'index_add_event': 'Добавить соревнование',
+    'auth': 'Авторизация',
 
+    'balloon_state_carrying': 'Несут',
+    'balloon_state_delivered': 'Доставлен',
+    'balloon_state_wanted': 'Нужно отнести!',
+
+    'event_header_monitor_link': 'Монитор соревнования',
+    'event_header_offer': 'Предлагаем отнести',
     'event_header_problems': 'Задачи',
     'event_header_queue': 'Очередь',
     'event_header_your_queue': 'Вы несете',
-    'event_header_offer': 'Предлагаем отнести',
-    'event_header_monitor_link': 'Монитор соревнования',
+    'event_queue_done': 'Готово',
+    'event_queue_drop': 'Отказаться',
+    'event_queue_first_solved': '1ok/t ',
+    'event_queue_first_to_solve': '1ok/p ',
     'event_queue_problem': 'Задача',
     'event_queue_team': 'Команда',
     'event_queue_take': 'Отнесу',
-    'event_queue_done': 'Готово',
-    'event_queue_drop': 'Отказаться',
-    'event_queue_first_to_solve': '1OK ',
-    'event_queue_first_solved': '1OK ',
+
+    'index_add_event': 'Добавить соревнование',
+    'index_log_in': 'Войти',
+    'index_monitor_url': 'URL монитора',
+    'index_no_events': 'Нет соревнований',
+    'index_not_authorised': 'Вы не авторизованы.',
+    'index_title': 'Шарики',
 
     'problem_cur_color': 'Цвет сейчас',
     'problem_set_color': 'Установить цвет',
-
-    'balloon_state_wanted': 'Нужно отнести!',
-    'balloon_state_carrying': 'Несут',
-    'balloon_state_delivered': 'Доставлен',
-
-    'auth': 'Авторизация',
 
     '': ''
 }
