fb1859075586004d2ef07ec93c1fd3c8587e5e0f
Oleg Davydov <burunduk3@gmail.com>
2017-12-02 23:28:55 +0300

    optimize, step #2

--- design.py
+++ design.py
@@ -5,32 +5,49 @@ from html import escape
 import lang
 
 
-def __wrap(s, raw=[]):
-    return lambda **kwargs: render_template_string(
-        s,
-        **{k: (v if k in raw else escape(v)) for k, v in kwargs.items()}
-    )
+# def __wrap(s, raw=[]):
+#    return lambda **kwargs: render_template_string(
+#        s,
+#        **{k: (v if k in raw else escape(v)) for k, v in kwargs.items()}
+#    )
 
 
-link = __wrap('<a href="{{url}}">{{label}}</a>\n')
+def link(*, url, label):
+    url = escape(url)
+    label = escape(label)
+    return '<a href="%s">%s</a>\n' % (url, label)
+
 def text(*, text):
     text = escape(text)
     return text + "\n"
-# text = __wrap('{{text}}\n')
-event_link = __wrap('<div><a href="{{url}}">{{name}}</a></div>\n')
-event_nolink = __wrap('<div><span>{{name}}</span></div>\n')
-event_add_form = __wrap(
-    '<div><form action="action{{token}}" method="POST">' +
-    '<input type="text" placeholder="' +
-    lang.lang['index_monitor_url'] + '" name="url" />' +
-    '<input type="submit" value="' +
-    lang.lang['index_add_event'] + '" />' +
-    '</form></div>\n'
-)
-error = __wrap(
-    '<h2 style="color: red;">{{message}}</h2>\n'
-    '<p><a href="{{back}}">' + lang.lang['back'] + '</a></p>\n'
-)
+
+def event_link(*, url, name):
+    url = escape(url)
+    name = escape(name)
+    return '<div><a href="%s">%s</a></div>\n' % (url, name)
+
+def event_nolink(*, name):
+    name = escape(name)
+    return '<div><span>%s</span></div>\n' % name
+
+def event_add_form(*, token):
+    token = escape(token)
+    return (
+        '<div><form action="action%s" method="POST">' +
+        '<input type="text" placeholder="' +
+        lang.lang['index_monitor_url'] + '" name="url" />' +
+        '<input type="submit" value="' +
+        lang.lang['index_add_event'] + '" />' +
+        '</form></div>\n'
+    ) % token
+
+def error(*, message, back):
+    message = escape(message)
+    back = escape(back)
+    return (
+        '<h2 style="color: red;">%s</h2>\n'
+        '<p><a href="%s">' + lang.lang['back'] + '</a></p>\n'
+    ) % (message, back)
 
 def action_link(*, token, label):
     token = escape(token)
@@ -42,45 +59,63 @@ def action_link(*, token, label):
         '</form>'
     ) % (token, token, token, label)
 
-action_link_raw = __wrap(
-    '<form action="action{{token}}" id="form{{token}}" method="POST">' +
-       '<span class="link" onclick="document.getElementById(\'form{{token}}\').submit();">' +
-       '{{label}}</span>' +
-    '</form>',
-    raw={'label'}
-)
-problem_header = __wrap('<h2>{{letter}}: {{name}}</h2>\n')
-problem_color = __wrap(
-    '<div><span style="color:{{color}}">' +
-    lang.lang['problem_cur_color'] +
-    ' <b>{{color}}</b></span></div>\n'
-)
-color_select = __wrap(
-    '<div>{{link}}</div>\n',
-    raw={'link'}
-)
-color_select_label = __wrap(
-    '<span style="color:{{color}}">' + lang.lang['problem_set_color'] +
-    ' <b>{{color}}</b></span>'
-)
-monitor_link = __wrap(
-    '<div><a href="{{url}}">' +
-    lang.lang['event_header_monitor_link'] + '</a></div>\n'
-)
-problems = __wrap(
-    '<h2>' + lang.lang['event_header_problems'] + '</h2>\n' +
-    '<table style="width: 100%;"><tr>' +
-    '{{problems}}' +
-    '</tr></table>\n',
-    raw={'problems'}
-)
-problem = __wrap(
-    '<td class="balloons_problem_color" style="' +
-    'background-color: {{color}};">{{color_token}}</td>\n' +
-    '<td class="balloons_problem_letter">' +
-    '<a href="{{url}}">{{letter}}</a>({{count}})</td>\n',
-    raw={'color_token'}
-)
+def action_link_raw(*, token, label):
+    token = escape(token)
+    return (
+        '<form action="action%s" id="form%s" method="POST">' +
+            '<span class="link" onclick="document.getElementById(\'form%s\').submit();">' +
+            '%s</span>' +
+        '</form>'
+    ) % (token, token, token, label)
+
+def problem_header(*, letter, name):
+    letter = escape(letter)
+    return '<h2>%s: %s</h2>\n' % (letter, name)
+
+def problem_color(*, color):
+    color = escape(color)
+    return (
+        '<div><span style="color:%s">' +
+        lang.lang['problem_cur_color'] +
+        ' <b>%s</b></span></div>\n'
+    ) % (color, color)
+
+def color_select(*, link):
+    return '<div>%s</div>\n' % link
+
+def color_select_label(*, color):
+    color = escape(color)
+    return (
+        '<span style="color:%s">' + lang.lang['problem_set_color'] +
+        ' <b>%s</b></span>'
+    ) % (color, color)
+
+def monitor_link(*, url):
+    url = escape(url)
+    return (
+        '<div><a href="%s">' +
+        lang.lang['event_header_monitor_link'] + '</a></div>\n'
+    ) % url
+
+def problems(*, problems):
+    return (
+        '<h2>' + lang.lang['event_header_problems'] + '</h2>\n' +
+        '<table style="width: 100%%;"><tr>' +
+        '%s' +
+        '</tr></table>\n'
+    ) % problems
+
+def problem(*, color, color_token, url, letter, count):
+    color = escape(color)
+    letter = escape(letter)
+    url = escape(url)
+    count = escape(count)
+    return (
+        '<td class="balloons_problem_color" style="' +
+        'background-color: %s;">%s</td>\n' +
+        '<td class="balloons_problem_letter">' +
+        '<a href="%s">%s</a>(%s)</td>\n'
+    ) % (color, color_token, url, letter, count)
 
 def fts(*, text):
     text = escape(text)
@@ -105,20 +140,34 @@ def balloon(*, color, color_token, problem_comment, letter, team_comment, team_s
         '<tr>\n'
     ) % (color, color_token, problem_comment, letter, team_comment, team_short, team, state)
 
-balloons = __wrap(
-    '<h2>{{header}}</h2>\n' +
-    '<table style="width: 100%;">\n' +
-    '{{balloons}}' +
-    '</table>\n',
-    raw={'balloons'}
-)
-auth = __wrap(
-    '<div>' + lang.lang['index_not_authorised'] +
-    ' <a href="{{url}}">' +
-    lang.lang['index_log_in'] +
-    '</a></div>\n'
-)
-auth_ok = __wrap('<div><b>{{user}}</b></div>\n')
-auth_link = __wrap('<div><a href="{{url}}">{{label}}</a></div>\n')
-volunteer = __wrap(' ({{id}})')
+def balloons(*, header, balloons):
+    header = escape(header)
+    return (
+        '<h2>%s</h2>\n' +
+        '<table style="width: 100%%;">\n' +
+        '%s' +
+        '</table>\n'
+    ) % (header, balloons)
+
+def auth(*, url):
+    url = escape(url)
+    return (
+        '<div>' + lang.lang['index_not_authorised'] +
+        ' <a href="%s">' +
+        lang.lang['index_log_in'] +
+        '</a></div>\n'
+    ) % url
+
+def auth_ok(*, user):
+    user = escape(user)
+    return '<div><b>%s</b></div>\n' % user
+
+def auth_link(*, url, label):
+    url = escape(url)
+    label = escape(label)
+    return '<div><a href="url">label</a></div>\n' % (url, label)
+
+def volunteer(*, id):
+    id = escape(id)
+    return ' ({%s)' % id
 
